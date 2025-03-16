666b377c902ed17002a996c59e75adac6652f5e8
Ega Alifya Firnando <eafirnando@genesysindonesia.com>
2025-03-16 10:18:10 +0700

    fix: adjustment cors in development and production mode

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -107,16 +107,19 @@ export default function Register() {
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                     <span className="bg-background text-muted-foreground relative z-10 px-2">Or sign up with</span>
                 </div>
-                <div className="grid grid-cols-3 gap-4">
-                    <a href="/auth/apple" className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={20} height={24}>
+                <div className="grid grid-cols-2 gap-4">
+                    {/* <a href="/auth/apple" className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2 dark:border-neutral-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={20} height={20}>
                             <path
                                 d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
                                 fill="currentColor"
                             />
                         </svg>
-                    </a>
-                    <a href="/auth/google" className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2">
+                    </a> */}
+                    <a
+                        href="/auth/google"
+                        className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2 dark:border-neutral-700"
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={19} height={19}>
                             <path
                                 d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -124,7 +127,10 @@ export default function Register() {
                             />
                         </svg>
                     </a>
-                    <a href="/auth/github" className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2">
+                    <a
+                        href="/auth/github"
+                        className="flex w-full items-center justify-center rounded-lg border border-neutral-200 px-4 py-2 dark:border-neutral-700"
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
                             <path
                                 d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
