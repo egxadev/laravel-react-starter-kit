fcd785ea661f5730af49394ad99f16214df2307c
Ega Alifya Firnando <eafirnando@genesysindonesia.com>
2025-05-18 21:53:05 +0700

    chore: change file name constant

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -7,7 +7,7 @@ import { NavigationMenu, NavigationMenuItem, NavigationMenuList, navigationMenuT
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { UserMenuContent } from '@/components/user-menu-content';
-import { footerNavItems, mainNavItems } from '@/constants/routes';
+import { footerNavItems, mainNavItems } from '@/constants/navigation';
 import { useInitials } from '@/hooks/use-initials';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type SharedData } from '@/types';
