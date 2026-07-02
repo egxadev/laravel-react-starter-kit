cdb8544db1ae82a3f3790aa7b80fad4d087605b2
Ega Alifya Firnando <eafirnando@genesysindonesia.com>
2026-07-02 12:24:41 +0700

    update: adjustment db migration

--- database/migrations/2024_01_01_000000_create_passkeys_table.php
+++ database/migrations/2024_01_01_000000_create_passkeys_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('passkeys', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id')->constrained()->cascadeOnDelete();
+            $table->uuid('user_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('credential_id')->unique();
             $table->json('credential');
